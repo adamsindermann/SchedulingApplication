27aadb86cf35fa0139823acc46dcdab06fcb80cf
Adam Sindermann <adamsindermann@gmail.com>
2021-03-31 11:44:40 -0400

    Renamed package

---
+++ src/DOA/DBCountry.java
@@ -0,0 +1,42 @@
+package DOA;
+
+import Database.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Country;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Adam Sindermann
+ */
+public class DBCountry {
+    
+    public static ObservableList<Country> getAllCountries(){
+        ObservableList<Country> clist = FXCollections.observableArrayList();
+        
+        try{
+            String sql = "SELECT * from countries";
+            
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                int countryId = rs.getInt("Country_ID");
+                String countryName = rs.getString("Country");
+                
+                
+                Country c = new Country(countryId, countryName);
+                clist.add(c);
+            }
+        } catch (SQLException e) { 
+            e.printStackTrace();
+        }
+        
+        return clist;
+    }
+    
+}
