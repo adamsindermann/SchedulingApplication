05dec85aa862ad5b7217f8f88c8d795461de840c
AdamS <AdamS@192.168.0.3>
2021-04-25 07:26:07 -0400

    Data now loads when editing Appointments

--- src/view/DashboardController.java
+++ src/view/DashboardController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 import DOA.DBAppointment;
@@ -26,9 +21,12 @@ import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalField;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import utility.Sort;
 import utility.TimeInterface;
 import utility.ToggleInterface;
@@ -57,7 +55,8 @@ public class DashboardController implements Initializable {
     private TableColumn<Appointment, String> appLocationCol;
     @FXML
     private TableColumn<Appointment, Integer> appContactCol;
-    @FXML private TableColumn<Appointment, String> appDateCol;
+    @FXML
+    private TableColumn<Appointment, String> appDateCol;
     @FXML
     private TableColumn<Appointment, String> appStartCol;
     @FXML
@@ -81,7 +80,7 @@ public class DashboardController implements Initializable {
     @FXML
     private RadioButton monthRadio;
     private ToggleGroup sortToggle;
-    
+
     //Customer TableView
     @FXML
     private TableView<Customer> customerView;
@@ -105,34 +104,36 @@ public class DashboardController implements Initializable {
     private Button editCustButton;
     @FXML
     private Tab custTab;
-    
+
     //Date Range Controller
-    @FXML private AnchorPane dateAnchor;
-    @FXML private Button dateLeft;
-    @FXML private Button dateRight;
-    @FXML private Label dateRangeLabel;
+    @FXML
+    private AnchorPane dateAnchor;
+    @FXML
+    private Button dateLeft;
+    @FXML
+    private Button dateRight;
+    @FXML
+    private Label dateRangeLabel;
     private LocalDateTime currentWeekStart;
     private Month currentMonth;
     private int year;
-    
-    
-
 
     //File locations
     private String apptWindow = "/view/Appointment.fxml";
+    private String custWindow = "/view/Customer.fxml";
 
     private final WindowInterface loader = location -> {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource(location));
         return loader;
     };
-    
+
     private final TimeInterface now = () -> {
         return LocalDateTime.now();
     };
-    
+
     private final ToggleInterface radio = radioButton -> {
-        if (sortToggle.getSelectedToggle().equals(radioButton)){
+        if (sortToggle.getSelectedToggle().equals(radioButton)) {
             return true;
         } else {
             return false;
@@ -143,74 +144,98 @@ public class DashboardController implements Initializable {
         WindowUtility.newWindowWait(loader.getLoader(apptWindow), "Appointment");
         appointmentView.setItems(DBAppointment.getAllAppointments());
     }
-    
-    public void toggleSwitch(){
 
-        if(radio.isSelected(allRadio)){
+    public void editAppointment() throws IOException {
+        if (!appointmentView.getSelectionModel().getSelectedItems().isEmpty()) {
+            FXMLLoader appointmentLoader =loader.getLoader(apptWindow);
+            Parent parent = appointmentLoader.load();
+
+            AppointmentController controller = appointmentLoader.getController();
+            controller.editAppointment(appointmentView.getSelectionModel().getSelectedItem());
+
+            Scene modifyPartScene = new Scene(parent);
+            Stage stage = new Stage();
+
+            stage.setScene(modifyPartScene);
+            stage.showAndWait();
+        } else {
+//            InputValidation.displayInputAlert("Please select a part to modify.");
+        }
+    }
+
+    public void launchCustomerWindow() throws IOException {
+        WindowUtility.newWindowWait(loader.getLoader(custWindow), "Customer");
+        customerView.setItems(DBCustomer.getAllCustomers());
+
+    }
+
+    public void toggleSwitch() {
+
+        if (radio.isSelected(allRadio)) {
             appointmentView.setItems(DBAppointment.getAllAppointments());
             dateAnchor.visibleProperty().set(false);
         }
-        if(radio.isSelected(weekRadio)){
+        if (radio.isSelected(weekRadio)) {
             dateAnchor.visibleProperty().set(true);
             dateRangeLabel.setText(Sort.getWeek(now.getTime()));
             currentWeekStart = Sort.getWeekStart(now.getTime());
             appointmentView.setItems(Sort.sortByWeek(now.getTime()));
         }
-        if(radio.isSelected(monthRadio)){
+        if (radio.isSelected(monthRadio)) {
             currentMonth = now.getTime().getMonth();
             year = now.getTime().getYear();
             dateAnchor.visibleProperty().set(true);
             dateRangeLabel.setText(currentMonth.toString() + " " + year);
             appointmentView.setItems(Sort.sortByMonth(currentMonth, year));
         }
-        
+
     }
-    
-    public void next(){
-        if(radio.isSelected(weekRadio)){
+
+    public void next() {
+        if (radio.isSelected(weekRadio)) {
             nextWeek();
         }
-        
-        if(radio.isSelected(monthRadio)){
+
+        if (radio.isSelected(monthRadio)) {
             nextMonth();
         }
     }
-    
-    public void last(){
-        if(radio.isSelected(weekRadio)){
+
+    public void last() {
+        if (radio.isSelected(weekRadio)) {
             lastWeek();
         }
-        
-        if(radio.isSelected(monthRadio)){
+
+        if (radio.isSelected(monthRadio)) {
             lastMonth();
         }
     }
-    
-    public void nextWeek(){
+
+    public void nextWeek() {
         LocalDateTime weekStart = currentWeekStart.plusDays(8);
         dateRangeLabel.setText(Sort.getWeek(weekStart));
         currentWeekStart = weekStart;
         appointmentView.setItems(Sort.sortByWeek(weekStart));
     }
-    
-    public void nextMonth(){
-        if(currentMonth.equals(Month.DECEMBER)){
+
+    public void nextMonth() {
+        if (currentMonth.equals(Month.DECEMBER)) {
             year++;
         }
         currentMonth = currentMonth.plus(1);
         dateRangeLabel.setText(currentMonth.toString() + " " + year);
         appointmentView.setItems(Sort.sortByMonth(currentMonth, year));
     }
-    
-    public void lastWeek(){
+
+    public void lastWeek() {
         LocalDateTime weekStart = currentWeekStart.minusDays(6);
         dateRangeLabel.setText(Sort.getWeek(weekStart));
         currentWeekStart = weekStart;
         appointmentView.setItems(Sort.sortByWeek(weekStart));
     }
-    
+
     public void lastMonth() {
-        if(currentMonth.equals(Month.JANUARY)){
+        if (currentMonth.equals(Month.JANUARY)) {
             year--;
         }
         currentMonth = currentMonth.minus(1);
@@ -244,7 +269,7 @@ public class DashboardController implements Initializable {
         appCustIDCol.setCellValueFactory(new PropertyValueFactory<Appointment, Integer>("customerID"));
 
         appointmentView.setItems(DBAppointment.getAllAppointments());
-        
+
         sortToggle = new ToggleGroup();
         allRadio.setToggleGroup(sortToggle);
         weekRadio.setToggleGroup(sortToggle);
